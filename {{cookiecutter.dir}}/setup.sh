dd05f29476b562a9f1d9f79eb8b4b9b523ad5292
Xednom <monde.lacanlalay@gmail.com>
2024-11-06 23:13:13 +0800

    added a new condition in setup.sh

--- {{cookiecutter.dir}}/setup.sh
+++ {{cookiecutter.dir}}/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Check if .env file exists, if not, create it by copying env.example
+if [ ! -f .env ]; then
+    echo "Creating .env file from env.example..."
+    cp env.example .env
+else
+    echo ".env file already exists. Skipping creation."
+fi
+
 echo "Locking dependencies without updating..."
 poetry lock --no-update
 
