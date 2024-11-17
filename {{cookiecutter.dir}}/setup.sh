3944e021a63602a60ddd0d397c536036ac1e3943
Xednom <monde.lacanlalay@gmail.com>
2024-11-17 12:57:32 +0800

    updated setup.sh so we don't run it twice

--- {{cookiecutter.dir}}/setup.sh
+++ {{cookiecutter.dir}}/setup.sh
@@ -11,14 +11,11 @@ fi
 echo "Locking dependencies without updating..."
 poetry lock --no-update
 
-echo "Starting Poetry shell..."
-poetry shell
-
 echo "Installing dependencies (excluding the project root package)..."
 poetry install --no-root
 
 echo "Applying database migrations..."
-python manage.py migrate
+poetry run python manage.py migrate
 
 echo "Starting the Django development server..."
-python manage.py runserver
+poetry run python manage.py runserver
